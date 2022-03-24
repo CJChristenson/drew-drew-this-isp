f1b418ea769101fea72f484e82b32306e19d568d
CJChristenson <cooper@christenson.org>
2022-03-23 20:33:31 -0500

    Created Navbar template and created base files for all pages. In addition, also made routing on main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template 
+from flask import Flask, render_template, request
 import os
 
 
@@ -6,10 +6,28 @@ template_dir = os.path.abspath('./website/')
 app = Flask(__name__, template_folder=template_dir)
 
 
+
 @app.route('/')
-def hello():
-      return render_template('home.html')
-  
+def home():
+      return render_template('index.html')
+
+@app.route('/howcpuswork')
+def howcpuswork():
+      return render_template('howcpuswork.html')
+@app.route('/how8bitwork')
+def how8bitwork():
+      return render_template('how8bitwork.html')
+@app.route('/cpuprogram')
+def cpuprogram():
+      return render_template('cpuprogram.html')
+
+@app.route('/stream')
+def stream():
+      return render_template('stream.html')
+
+@app.route('/faq')
+def faq():
+      return render_template('faq.html')
 
 
 if __name__ == "__main__":
