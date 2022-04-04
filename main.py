128e39ed8f1b1272cc2f7e059e2a703e4014e997
CJChristenson <cooper@christenson.org>
2022-04-04 11:56:58 -0500

    Started testing for API

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #import flask libraries and os libraries 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify, make_response, Response, redirect, url_for
 import os
+import json
+from json import JSONEncoder
 
 #define the template directory (html pages) to be the website directory.
 template_dir = os.path.abspath('./website/')
@@ -22,7 +24,7 @@ def how8bitwork():
       return render_template('how8bitwork.html')
 @app.route('/cpuprogram')
 def cpuprogram():
-      return render_template('cpuprogram.html')
+      return render_template('cpuprogram.html', result="")
 
 @app.route('/stream')
 def stream():
@@ -32,7 +34,56 @@ def stream():
 def faq():
       return render_template('faq.html')
 
+#define class for one instruction
+class instruction():
+      def __init__(self, address, instruct, value):
+            self.address = address
+            self.instruct = instruct
+            self.value = value
 
+      def get_instruct(self):
+            return "%s %s %s" % (self.address, self.instruct, self.value)
+      def serialize(self):
+            return {
+                  'address': self.address,
+                  'instruction': self.instruct,
+                  'value': self.value
+            }
+
+instruct0 = instruction('0000','NOP','0000')
+instruct1 = instruction('0001','NOP','0000')
+#define class that is created to contain one set of instructions
+class program():
+      def __init__(self, id, position, program):
+            self.id = id,
+            self.position = position
+            self.program = program
+
+      def serialize(self):
+            return {
+                  'id': self.id,
+                  'position': self.position,
+                  'program': self.program.serialize()
+            }
+
+class programEncoder(JSONEncoder):
+      def default(self, o):
+            return o.__dict__
+            
+temp = program(1, 1, [instruct0, instruct1])
+
+test = ""
+@app.route('/api/test', methods=['POST'])
+def i():
+      if request.method == 'POST':
+            global test
+            test = request.form
+            return redirect(url_for('.cpuprogram', result="Success!"))
+     
+
+@app.route('/test')
+def t():
+      return test
 #This will call Flask to run the application to run on the VAPOR link. If the file name is main, it will run the application
 if __name__ == "__main__":
         app.run(host='0.0.0.0', port=(os.environ.get('VAPOR_LOCAL_PORT')))
