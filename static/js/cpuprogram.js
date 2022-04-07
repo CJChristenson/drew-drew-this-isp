c33aad5b8b2070075fe7696ed03ad96b0e65927f
CJChristenson <cooper@christenson.org>
2022-04-07 14:10:23 -0500

    Added API handling to cpuprogram.html

--- static/js/cpuprogram.js
+++ static/js/cpuprogram.js
@@ -1,3 +1,4 @@
+var id = 0
 document.addEventListener('submit', (e) => {
     // Store reference to form to make later code easier to read
     const form = e.target;
@@ -7,19 +8,27 @@ document.addEventListener('submit', (e) => {
 	body: new FormData(form),
     })
 	.then(response => {
-	    console.log(response.ok);
+	   // console.log(response.ok);
+	    
 	    if (response.ok){
 		document.getElementById("input-form").reset();
 		document.getElementById("success").hidden = false;
 		document.getElementById("failure").hidden = true;
 		setTimeout(() => {document.getElementById("success").hidden = true;}, 2000);
+		
 	    } else {
 		document.getElementById("failure").hidden = false;
 		document.getElementById("success").hidden = true;
 		setTimeout(() => {document.getElementById("failure").hidden = true;}, 2000);
 	    }
+	    return response.json()
+	    
 	})
-/*
+	.then(data => {
+	    id = data.id;
+	    document.getElementById("position-label").innerHTML = `Your position in queue: ${data.position}`;
+	})
+    /*
     console.log(response);
     if (response == "200") {
 	    document.getElementById("input-form").reset();
