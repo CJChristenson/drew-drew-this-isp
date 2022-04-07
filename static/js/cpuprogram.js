bac52c0b8d84041737c61d008a398e1827ad229b
CJChristenson <cooper@christenson.org>
2022-04-07 11:01:16 -0500

    Added table submit functionlity and added success/failure messages

---
+++ static/js/cpuprogram.js
@@ -0,0 +1,31 @@
+document.addEventListener('submit', (e) => {
+    // Store reference to form to make later code easier to read
+    const form = e.target;
+    var response = ""
+    fetch(form.action, {
+	method: form.method,
+	body: new FormData(form),
+    })
+	.then(response => {
+	    console.log(response.ok);
+	    if (response.ok){
+		document.getElementById("input-form").reset();
+		document.getElementById("success").hidden = false;
+		document.getElementById("failure").hidden = true;
+		setTimeout(() => {document.getElementById("success").hidden = true;}, 2000);
+	    } else {
+		document.getElementById("failure").hidden = false;
+		document.getElementById("success").hidden = true;
+		setTimeout(() => {document.getElementById("failure").hidden = true;}, 2000);
+	    }
+	})
+/*
+    console.log(response);
+    if (response == "200") {
+	    document.getElementById("input-form").reset();
+	}
+  */  
+
+    // Prevent the default form submit
+    e.preventDefault();
+});
