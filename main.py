dde26d09205e377662605a3d6cc4155a5403048a
CJChristenson <cooper@christenson.org>
2022-04-18 14:59:11 -0500

    Added comments for clarity on main.py

--- main.py
+++ main.py
@@ -12,24 +12,32 @@ app = Flask(__name__, template_folder=template_dir)
 
 
 #create routes for the certain pages. When a user travels to the URL contained withing the quotes, it will call the function.
+#index route that brings user to home page.
 @app.route('/')
 def home():
       return render_template('index.html')
 
+#route that brings user to the How CPU's work page
 @app.route('/howcpuswork')
 def howcpuswork():
       return render_template('howcpuswork.html')
+
+#route that brings user to the How 8-bit CPU's work page
 @app.route('/how8bitwork')
 def how8bitwork():
       return render_template('how8bitwork.html')
+
+#route that brings user to the Programming Page
 @app.route('/cpuprogram')
 def cpuprogram():
       return render_template('cpuprogram.html', result="")
 
+#route that brings user to page that only displays the stream.
 @app.route('/stream')
 def stream():
       return render_template('stream.html')
 
+#route that brings user to page that has FAQs.
 @app.route('/faq')
 def faq():
       return render_template('faq.html')
@@ -50,8 +58,8 @@ class instruction():
                   'value': self.value
             }
 
-instruct0 = instruction('0000','NOP','0000')
-instruct1 = instruction('0001','NOP','0000')
+
+
 #define class that is created to contain one set of instructions
 class program():
       def __init__(self, id, program):
@@ -63,30 +71,33 @@ class program():
                   'id': self.id,
                   'program': self.program.serialize()
             }
-
+#programEncoder class for JSON encoding
 class programEncoder(JSONEncoder):
       def default(self, o):
             return o.__dict__
             
-temp = program(1, [instruct0, instruct1])
 
-programs = []
-ids = []
-currentId = 1
 
+programs = [] #array that holds all programs yet to be executed
+ids = [] #array to hold list of all ID's. The index of an ID is it's position in queue.
+currentId = 1 #value to keep track of next ID to be assigned.
+
+#function that if a value is nothing, changes it to 0000.
 def processInputVal(inp):
       if (inp == ""):
             return "0000"
       else:
             return inp
 
+#function that if a instruction is nothing, it is changed to NOP.      
 def processInputInstruct(inst):
       if (inst == ""):
             return "NOP"
       else:
             return inst
 
-      
+#API URL for creating a new program. It is a POST method. It takes a request, and processes all the information to create a
+#instance of a program and then returns the id and it's position.
 @app.route('/api/new-program', methods=['POST'])
 def i():
       global currentId
@@ -111,6 +122,7 @@ def i():
             #ids.pop(0)
             return jsonify(id=newProgram.id, position=ids.index(newProgram.id))
 
+#API URL for returning the current program. It is a GET method. 
 @app.route('/api/current-program')
 def current_program():
       if (len(programs) >= 1):
@@ -119,14 +131,14 @@ def current_program():
             return json.dumps("No programs")
             
       
-
+#API URL for removing the current program. It is a GET method
 @app.route('/api/remove-program')
 def remove_program():
       programs.pop(0)
       ids.pop(0)
       return "Success"
       
-
+#API URL for getting the position in queue of an ID. It is a GET method that takes a ID value in.
 @app.route('/api/position')
 def position():
       args = request.args
